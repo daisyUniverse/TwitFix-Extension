95daad0759bdf82074931e6845b2f4c68b22d555
Doruk Yılmaz <dorukyogscast@gmail.com>
2022-01-17 01:50:07 +0300

    v1.1 fixed the issue with main pages

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+function specparent(a, b) { //element and tag to find
+    for (b = b.toLowerCase(); a && a.parentNode;)
+        if (a = a.parentNode, a.tagName && a.tagName.toLowerCase() == b) return a;
+    return null
+}
 document.addEventListener('contextmenu', (e) => {
-    if (document.querySelector("div[data-testid='videoPlayer']").contains(e.target)) {
+    const varray = Array.from(document.querySelectorAll("div[data-testid='videoPlayer']")).filter(i => (i.contains(e.target)));
+    if (varray.length > 0) {
+        const menu = varray[0];
+        const url = Array.from(specparent(menu, "article").querySelectorAll("a")).filter(a => (a.href.includes("status")))[0].href;
+
         function change() {
-            const flex = document.querySelector("div[role='menuitem']");
+            const flex = menu.querySelector("div[role='menuitem']");
             if (!flex.parentElement.contains(flex.parentElement.querySelector(".fxtwitterclass"))) {
                 const css = `
                 .fxtwitterclass{
@@ -33,19 +42,19 @@ document.addEventListener('contextmenu', (e) => {
                 opt1.querySelector("span").textContent = "Copy Twitfix Link";
                 opt1.classList.add("fxtwitterclass");
                 opt1.addEventListener("click", () => {
-                    navigator.clipboard.writeText(window.location.href.replace("https://", "https://fx"));
+                    navigator.clipboard.writeText(url.replace("https://", "https://fx"));
                 });
                 opt2.querySelector("span").textContent = "Copy MP4 Link";
                 opt2.classList.add("fxtwitterclass");
                 opt2.addEventListener("click", () => {
-                    navigator.clipboard.writeText(window.location.href.replace("https://", "https://fx").concat(".mp4"));
+                    navigator.clipboard.writeText(url.replace("https://", "https://fx").concat(".mp4"));
                 });
                 flex.parentElement.append(opt1);
                 flex.parentElement.append(opt2);
             }
         }
         let inter = setInterval(() => {
-            if (document.querySelector("div[role='menuitem']") != null) {
+            if (menu.querySelector("div[role='menuitem']") != null) {
                 change();
                 clearInterval(inter);
             }
