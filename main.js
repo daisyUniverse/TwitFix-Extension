40a2dd68f99918fbc9ca90b5822f80e671111380
Doruk Yılmaz <dorukyogscast@gmail.com>
2022-01-17 11:11:20 +0300

    v1.2 added better hover

--- main.js
+++ main.js
@@ -6,51 +6,47 @@ function specparent(a, b) { //element and tag to find
 document.addEventListener('contextmenu', (e) => {
     const varray = Array.from(document.querySelectorAll("div[data-testid='videoPlayer']")).filter(i => (i.contains(e.target)));
     if (varray.length > 0) {
+        const css = `
+            .fxtwitterclass {
+                background-color: unset;
+            }
+            
+            body[style*="background-color: rgb(0, 0, 0)"] .fxtwitterclass:hover,
+            body[style*="background-color: #15202B"] .fxtwitterclass:hover {
+                background-color: rgba(255, 255, 255, 0.03);
+            }
+            
+            body[style*="background-color: rgb(255, 255, 255)"] .fxtwitterclass:hover {
+                background-color: rgba(0, 0, 0, 0.03);
+            }
+        `;
+        const style = document.createElement('style');
+        style.styleSheet ? style.styleSheet.cssText = css : style.appendChild(document.createTextNode(css));
         const menu = varray[0];
         const url = Array.from(specparent(menu, "article").querySelectorAll("a")).filter(a => (a.href.includes("status")))[0].href;
 
         function change() {
-            const flex = menu.querySelector("div[role='menuitem']");
-            if (!flex.parentElement.contains(flex.parentElement.querySelector(".fxtwitterclass"))) {
-                const css = `
-                .fxtwitterclass{
-                    text-align: center; 
-                    cursor: pointer;
-                    -webkit-box-direction: normal;
-                    -webkit-box-orient: horizontal;
-                    flex-direction: row;
-                    transition-duration: 0.2s;
-                    outline-style: none;
-                    transition-property: background-color, box-shadow;
-                    width: 100%;
-                    padding: 16px;
-                }
-                .fxtwitterclass:hover{
-                    background-color: rgba(255, 255, 255, 0.03);
-                }
-                `;
-                const style = document.createElement('style');
-                if (style.styleSheet) {
-                    style.styleSheet.cssText = css;
-                } else {
-                    style.appendChild(document.createTextNode(css));
-                }
+            const ogitem = menu.querySelector("div[role='menuitem']");
+            const flex = ogitem.parentElement;
+            if (!flex.contains(flex.querySelector(".fxtwitterclass"))) {
+
                 document.getElementsByTagName('head')[0].appendChild(style);
-                const ogitem = flex.children[0];
                 const opt1 = ogitem.cloneNode(true);
                 const opt2 = ogitem.cloneNode(true);
                 opt1.querySelector("span").textContent = "Copy Twitfix Link";
                 opt1.classList.add("fxtwitterclass");
+                //opt1.classList.remove("r-1panhkp");
                 opt1.addEventListener("click", () => {
                     navigator.clipboard.writeText(url.replace("https://", "https://fx"));
                 });
                 opt2.querySelector("span").textContent = "Copy MP4 Link";
                 opt2.classList.add("fxtwitterclass");
+                //opt2.classList.remove("r-1panhkp");
                 opt2.addEventListener("click", () => {
                     navigator.clipboard.writeText(url.replace("https://", "https://fx").concat(".mp4"));
                 });
-                flex.parentElement.append(opt1);
-                flex.parentElement.append(opt2);
+                flex.append(opt1);
+                flex.append(opt2);
             }
         }
         let inter = setInterval(() => {
