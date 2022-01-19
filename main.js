156030e38e21e14e32be844123184cae7672fbac
Doruk Yılmaz <dorukyogscast@gmail.com>
2022-01-19 19:43:24 +0300

    Bunch of fixes Fix for quote tweets, avoids em entirely. Broadcasts are avoided as well

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ function specparent(a, b) { //returns parent element with specific tag, a is chi
 }
 document.addEventListener('contextmenu', (e) => { //event for right click
     const varray = Array.from(document.querySelectorAll("div[data-testid='videoPlayer']")).filter(i => i.contains(e.target)); //this is array of targets
-    if (varray.length > 0) { //checks if rclick was over video player 
+    const videoPlayer = varray[0];
+    const article = specparent(videoPlayer, "article");
+    const atags = Array.from(article.querySelectorAll("a"));
+    const url = atags.filter(a => a.href.includes("status"))[0].href; //url is pulled from a A tag inside the tweet (article)
+    if (varray.length > 0 && atags.filter(a => a.href.includes("broadcasts")).length < 1 && videoPlayer != article.querySelector("div[role=\"link\"] [data-testid=\"videoPlayer\"]")) { //checks if rclick was over video player 
         const css = ` 
             .fxtwitterclass {
                 background-color: unset;
@@ -18,15 +22,12 @@ document.addEventListener('contextmenu', (e) => { //event for right click
             body[style*="background-color: #FFFFFF"] .fxtwitterclass:hover {
                 background-color: rgba(0, 0, 0, 0.03);
             }
-        `; //stylesheet for the new rclick items
+        `; //stylesheet for the new rclick items, it is best to keep this here so twitter doesnt overwrite it
         const style = document.createElement('style'); //creates style el
         style.styleSheet ? style.styleSheet.cssText = css : style.appendChild(document.createTextNode(css));
         document.getElementsByTagName('head')[0].appendChild(style); //appends to head
-        const menu = varray[0];
-        const url = Array.from(specparent(menu, "article").querySelectorAll("a")).filter(a => a.href.includes("status"))[0].href; //url is pulled from a A tag inside the tweet (article)
-
         function change() { //function for simplicity
-            const ogitem = menu.querySelector("div[role='menuitem']"); //original item <> Copy Video Adress
+            const ogitem = videoPlayer.querySelector("div[role='menuitem']"); //original item <> Copy Video Adress
             const flex = ogitem.parentElement; //container/parent
             if (!flex.contains(flex.querySelector(".fxtwitterclass"))) { //checks if fxtwitter items are already there
                 const opt1 = ogitem.cloneNode(true); //clones a copy of ogitem
@@ -42,13 +43,11 @@ document.addEventListener('contextmenu', (e) => { //event for right click
             }
         }
         //Interval that checks every 4ms after rclick, waits until twitter's context menu appears then applies change and finally stops
-        /*Todo: Find a better way to handle this, current imp. can cause potential bugs:
-            For example two rclicks can be triggered same time causing wrong items being added to twitter context
-        */
+        //Todo: Find a better way to handle this, current imp. can cause potential bugs
         var step = 0; //added so it stops after a while
         let inter = setInterval(() => {
             step += 1;
-            if (menu.querySelector("div[role='menuitem']") != null) { //checks if twitter's context menu exists
+            if (videoPlayer.querySelector("div[role='menuitem']") != null) { //checks if twitter's context menu exists
                 change();
                 clearInterval(inter);
                 step = 0;
